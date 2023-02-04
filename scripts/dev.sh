b9569bac339825f0704ddcc204c860fe72aacd6e
Patrick Canfield <pscale01@gmail.com>
2023-02-04 12:28:03 -0800

    Update dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ./src/index.html ./public
-deno bundle --watch ./src/client/mod.ts ./public/client.bundle.js &
-deno run --allow-net --allow-read --watch ./src/server/mod.ts
+cp ./src/index.html ./public \
+  && deno bundle --watch ./src/client/mod.ts ./public/client.bundle.js \
+  && deno run --allow-net --allow-read --watch ./src/server/mod.ts
