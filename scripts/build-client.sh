c75eaf74cf0764b7149f8d42b3cb8986224e0ff3
Patrick Canfield <pscale01@gmail.com>
2023-02-05 09:38:39 -0800

    try absolute paths

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p ./dist/public \
-  && cp ./src/index.html ./dist/public \
-  && deno bundle ./src/client/mod.ts ./dist/public/client.bundle.js
+mkdir -p /dist/public \
+  && cp /src/index.html /dist/public \
+  && deno bundle /src/client/mod.ts /dist/public/client.bundle.js
