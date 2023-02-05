405d4c26edd2f2c94e8d9da4bebbf2d848d6eb65
Patrick Canfield <pscale01@gmail.com>
2023-02-05 09:39:41 -0800

    Revert "try absolute paths"
    This reverts commit c75eaf74cf0764b7149f8d42b3cb8986224e0ff3.

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /dist/server \
-  && cp /src/server /dist -a \
-  && cp /src/common /dist -a
+mkdir -p ./dist/server \
+  && cp ./src/server ./dist -a \
+  && cp ./src/common ./dist -a
