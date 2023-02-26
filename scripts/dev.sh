96d6ecc41658e04750a22b85a29b54bc0405a6f0
Ronald Corona <Ronaldmcorona@gmail.com>
2023-02-25 16:48:49 -0800

    fix scripts

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir public
 cp ./src/index.html ./public \
   && deno bundle --watch ./src/client/mod.ts ./public/client.bundle.js &
 deno run --allow-net --allow-read --watch ./src/server/mod.ts
