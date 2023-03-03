5703395786d58d1a0353fd9b5a1396181304cb81
Patrick Canfield <pscale01@gmail.com>
2023-03-02 18:19:33 -0800

    modules swapping is so hot rn

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cp ./src/index.html ./public \
-  && deno bundle --watch ./src/client/mod.ts ./public/client.bundle.js &
+cp ./src/index.html ./public
+deno bundle --watch ./src/client/mod.ts ./public/client.bundle.js &
+deno bundle --watch ./src/common/dev_tools/client.ts ./public/dev_tools.bundle.js &
 deno run --allow-net --allow-read --watch ./src/server/mod.ts
