83bf6ea305840b9a2341e726ddf93e1378eeb5ac
Patrick Canfield <pscale01@gmail.com>
2023-03-03 13:14:53 -0800

    dev script exits on critical error

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+function assert_ok() {
+  if (( $? != 0 )); then
+    echo "Assertion failed: Previous command exited with non-zero status. Exiting..."
+    exit
+  fi
+}
+
+mkdir public
 cp ./src/index.html ./public
+assert_ok
 deno bundle --watch ./src/client/mod.ts ./public/client.bundle.js &
+assert_ok
 deno bundle --watch ./src/common/dev_tools/client.ts ./public/dev_tools.bundle.js &
+assert_ok
 deno run --allow-net --allow-read --watch ./src/server/mod.ts
+assert_ok
+
