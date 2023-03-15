2ec1adb6aca506755122b88456df08227bffa5fe
Patrick Canfield <pscale01@gmail.com>
2023-03-15 11:59:42 -0700

    fix build again

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -5,7 +5,7 @@ source ./scripts/common.sh
 function build_client_modules() {
   for in_path in $client_sub_module_rel_paths; do
     out_path=$(get_out_path_for_client_module "$in_path")
-    npx tsc "$in_path" --outDir "$out_path" --lib esnext,dom --target esnext --module es2020
+    npx tsc "$in_path" --outDir "./dist/$out_path" --lib esnext,dom --target esnext --module es2020
   done
 }
 
