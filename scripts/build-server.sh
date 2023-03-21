9b83ef7455dcdbb297e750681d0c6a258fd03d76
Patrick Canfield <pscale01@gmail.com>
2023-03-21 12:37:34 -0700

    fix build

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p ./dist/modules \
+  && cp -a ./src/examples ./dist/examples \
   && cp -a ./src/modules/server ./dist/modules \
   && cp -a ./src/modules/common ./dist/modules \
   && cp ./import_map.json ./dist
