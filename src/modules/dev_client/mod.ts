2e82093c0400275622ecb1c6621eb231035ea31b
Patrick Canfield <pscale01@gmail.com>
2023-03-31 16:52:26 -0700

    prevent errors from stopping dev build

--- src/modules/dev_client/mod.ts
+++ src/modules/dev_client/mod.ts
@@ -1,26 +1,47 @@
-import { basename, dirname } from 'path'
-import * as esbuild from 'https://deno.land/x/esbuild@v0.17.12/mod.js'
+import { basename, dirname } from "path";
+import * as esbuild from "https://deno.land/x/esbuild@v0.17.12/mod.js";
 
 function getOutPath(outDir: string, inPath: string) {
-  const segments = inPath.split('/')
-  const keepSegments = segments.slice(1, -1)
-  const baseName = segments[segments.length - 1]
-  const baseNameNoExt = basename(baseName, ".ts")
-  return `${outDir}/${keepSegments.join('/')}/${baseNameNoExt}.js`
+  const segments = inPath.split("/");
+  const keepSegments = segments.slice(1, -1);
+  const baseName = segments[segments.length - 1];
+  const baseNameNoExt = basename(baseName, ".ts");
+  return `${outDir}/${keepSegments.join("/")}/${baseNameNoExt}.js`;
 }
 
-export async function buildModules(outDir: string, inPaths: Array<string>) {
-  for(const inPath of inPaths) {
-    await buildModule(outDir, inPath)
+interface BuildOptions {
+  catchErrors?: boolean;
+}
+
+export async function buildModules(
+  outDir: string,
+  inPaths: Array<string>,
+  options: BuildOptions = {},
+) {
+  for (const inPath of inPaths) {
+    await buildModule(outDir, inPath, options);
   }
 }
 
-export async function buildModule(outDir: string, inPath: string) {
-  const outPath = getOutPath(outDir, inPath)
-  Deno.mkdir(dirname(outPath), {recursive: true})
-  const ts = await Deno.readTextFile(inPath)
-  const result = await esbuild.transform(ts, { loader: 'ts' })
-  esbuild.stop()
-  console.log(`building ${inPath} => ${outPath}`)
-  Deno.writeTextFile(outPath, result.code)
+export async function buildModule(
+  outDir: string,
+  inPath: string,
+  options: BuildOptions = {},
+) {
+  const outPath = getOutPath(outDir, inPath);
+  Deno.mkdir(dirname(outPath), { recursive: true });
+  const ts = await Deno.readTextFile(inPath);
+  try {
+    console.log(`building ${inPath} => ${outPath}`);
+    const result = await esbuild.transform(ts, { loader: "ts" });
+    Deno.writeTextFile(outPath, result.code);
+  } catch (e) {
+    if (options.catchErrors) {
+      console.error(e);
+    } else {
+      throw e;
+    }
+  } finally {
+    esbuild.stop();
+  }
 }
