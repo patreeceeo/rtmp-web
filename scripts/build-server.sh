1b2c3c1d24cdb1c3b9b59ff116f530426a5d130c
Patrick Canfield <pscale01@gmail.com>
2023-04-08 09:01:05 -0700

    include client code in server
    in order to make it easier to write isomorphic code that statically imports things from the client

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -3,4 +3,5 @@ mkdir -p ./dist/modules \
   && cp -a ./src/examples ./dist/examples \
   && cp -a ./src/modules/server ./dist/modules \
   && cp -a ./src/modules/common ./dist/modules \
+  && cp -a ./src/modules/client ./dist/modules \
   && cp ./import_map_prod.json ./dist
