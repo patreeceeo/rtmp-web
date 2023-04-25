3476ccf89700b4c7fda22ff1bccfdc50252f48fa
Patrick Canfield <pscale01@gmail.com>
2023-04-25 12:12:08 -0700

    Revert "try sourcemaps in prod"
    This reverts commit 16248d69876a3b5e70bcd92b7737ab0083794e8b.

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -3,7 +3,7 @@
 source ./scripts/common.sh
 
 function build_client_modules() {
-  ./scripts/build_client.ts "dist/public" "$(join_array , $client_sub_module_rel_paths)" import_map_client.json --inline-sourcemap
+  ./scripts/build_client.ts "dist/public" "$(join_array , $client_sub_module_rel_paths)" import_map_client.json
 }
 
 mkdir -p ./dist/public \
