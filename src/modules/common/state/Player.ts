e11374e0d7bec02a613d48217f296369e8b9dbe1
Patrick Canfield <pscale01@gmail.com>
2023-05-18 16:17:10 -0700

    clean up player code

--- src/modules/common/state/Player.ts
+++ src/modules/common/state/Player.ts
@@ -3,26 +3,6 @@ import { defaultWorld, EntityId } from "./mod.ts";
 import * as ECS from "bitecs";
 import { BoxReadOnly } from "../Box.ts";
 
-export enum ColorId {
-  RED,
-  ORANGE,
-  YELLOW,
-  GREEN,
-  BLUE,
-  INDIGO,
-  VIOLET,
-}
-
-const webColors = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "indigo",
-  "violet",
-];
-
 const hitBox = new BoxReadOnly(0, 0, 16 * 256, 32 * 256);
 
 export class Player {
@@ -72,45 +52,12 @@ export class Player {
     EntityStore.eid[this.eid] = eid;
   }
 
-  get timeWarp(): number {
-    return TimeWarpStore.value[this.eid];
-  }
-
-  set timeWarp(value: number) {
-    TimeWarpStore.value[this.eid] = value;
-  }
-
-  get color(): ColorId {
-    return ColorStore.value[this.eid] as ColorId;
-  }
-
-  set color(cid: ColorId) {
-    ColorStore.value[this.eid] = cid;
-  }
-
-  get webColor() {
-    return webColors[this.color];
-  }
-
   get pose() {
     return PoseStore.value[this.eid] as PoseType;
   }
 
   set pose(pose: PoseType) {
     PoseStore.value[this.eid] = pose;
-  }
-
-  get snapshot() {
-    return {
-      eid: this.eid,
-      position: this.position.snapshot,
-      lastActiveTime: this.lastActiveTime,
-    };
-  }
-
-  applySnapshot(snap: typeof this.snapshot) {
-    this.position.applySnapshot(snap.position);
-    this.lastActiveTime = snap.lastActiveTime;
   }
 }
 
@@ -123,7 +70,6 @@ const EntityStore = ECS.defineComponent({ eid: ECS.Types.ui32 });
 const LastActiveStore = ECS.defineComponent({ time: ECS.Types.ui32 });
 const ColorStore = ECS.defineComponent({ value: ECS.Types.ui8 });
 const PoseStore = ECS.defineComponent({ value: ECS.Types.ui8 });
-const TimeWarpStore = ECS.defineComponent({ value: ECS.Types.i8 });
 
 export enum PoseType {
   facingLeft,
@@ -146,7 +92,6 @@ class PlayerStateApi {
     ECS.addComponent(this.world, LastActiveStore, eid);
     ECS.addComponent(this.world, ColorStore, eid);
     ECS.addComponent(this.world, PoseStore, eid);
-    ECS.addComponent(this.world, TimeWarpStore, eid);
     return player;
   }
 
@@ -174,38 +119,6 @@ class PlayerStateApi {
   getPlayers(): Array<Player> {
     return this.getEntityIds().map((eid) => this.getPlayer(eid));
   }
-
-  get snapshot() {
-    const snap: Array<typeof Player.prototype.snapshot> = [];
-    for (const player of PlayerState.getPlayers()) {
-      snap[player.eid] = player.snapshot;
-    }
-    return snap;
-  }
-
-  applySnapshot(snap: typeof this.snapshot) {
-    for (const playerSnapshot of snap) {
-      this.getPlayer(playerSnapshot.eid).applySnapshot(playerSnapshot);
-    }
-  }
-
-  /**
-  * work in progress
-  getDiff(before: typeof this.snapshot, after: typeof this.snapshot) {
-    const diff = {
-      create: {},
-      update: {},
-      delete: [] as EntityId[],
-    }
-    const longestKeys = before.length > after.length ? before.keys() : after.keys()
-
-    for(const key of longestKeys) {
-      if(key in before && !(key in after)) {
-        diff.delete.push(key as EntityId)
-      }
-    }
-  }
-  */
 }
 
 export const PlayerState = new PlayerStateApi();
