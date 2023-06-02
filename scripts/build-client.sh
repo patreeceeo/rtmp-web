6dff9a4b1e7418d023e3471cebc24318087a4ec3
Patrick Canfield <pscale01@gmail.com>
2023-06-01 17:36:00 -0700

    heroku updates

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -3,10 +3,10 @@
 source ./scripts/common.sh
 
 function build_client_modules() {
-  ./scripts/build_client.ts "dist/public" "$(join_array , $client_sub_module_rel_paths)" import_map_client.json
+  ./scripts/build_client.ts "public" "$(join_array , $client_sub_module_rel_paths)" import_map_client.json
 }
 
-mkdir -p ./dist/public \
-  && cp ./src/index.html ./dist/public \
-  && cp "./src/examples/$active_project/assets" ./dist/public/assets -a \
+mkdir -p ./public \
+  && cp ./src/index.html ./public \
+  && cp "./src/examples/$active_project/assets" ./public/assets -a \
   && build_client_modules
