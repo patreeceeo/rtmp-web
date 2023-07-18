2c8e55d1d2a997a6267c05cb8d50546f2e2a3fc3
Patrick Canfield <pscale01@gmail.com>
2023-07-17 18:41:24 -0700

    comment

--- src/modules/common/state/Network.ts
+++ src/modules/common/state/Network.ts
@@ -3,6 +3,8 @@ import { ServerNetworkState } from "../../server/state/Network.ts";
 import { isClient } from "../env.ts";
 import { NetworkStateApi } from "../NetworkApi.ts";
 
+// TODO create Network Component that stores at least the nid?
+
 export type { NetworkId } from "../NetworkApi.ts";
 export { networkId } from "../NetworkApi.ts";
 
